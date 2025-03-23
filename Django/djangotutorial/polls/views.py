93fbddd4876564235d55ec2ebde6dd335b28f40f
Nixir-hub <nixir@hotmail.com>
2025-03-23 17:33:11 +0100

    5th commit

--- Django/djangotutorial/polls/views.py
+++ Django/djangotutorial/polls/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.db.models import F
 from django.template import loader
-from .models import Question
+from django.urls import reverse
+
+from .models import Question, Choice
+
 
 
 def index(request):
@@ -10,14 +14,27 @@ def index(request):
     context = {
         "latest_question_list": latest_question_list,
     }
-    return HttpResponse(template.render(context, request))
+    return render(request, "polls/index.html", context)
 # Create your views here.
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
 
 def results(request, question_id):
     response = "You're looking at the result for question %s."
     return HttpResponse(response % question_id)
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    questiom = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = questiom.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+
+        return render(request, "polls/detail.html", {
+        "question": questiom,
+        "error_message": "You didn't select a choice.",
+    },)
+    else:
+        selected_choice.votes = F("votes") + 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse("polls:results", args=(question_id,)))
